fd1e80b598ccec1f72dc2ccbbc1d9b301a3cb277
Caolei <caolei750cl@163.com>
2019-03-14 18:32:15 +0800

    first commit

---
+++ app/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+var app = new Vue({
+	el: "#app",
+	data: {
+		msg: "hello vue",
+		logo: '../images/logo.png'
+	}
+})
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+module.exports = {
+	devtool: 'eval-source-map',
+	entry: __dirname + '/app/main.js',
+	output: {
+		path: __dirname + '/dist',
+		filename: 'demo.js'
+	},
+	devServer: {
+		port: 3001,
+		contentBase: './dist',
+		inline: true
+	},
+	module: {
+		rules: [
+			{
+				test: /(\.jsx|\.js)$/,
+				use: {
+					loader: "babel-loader"
+				},
+				exclude: /node_modules/
+			},
+			{
+				test: /\.css$/,
+				use: [
+					{
+						loader: 'style-loader'
+					},
+					{
+						loader: 'css-loader',
+						options: {
+							modules: true,
+							localIdentName: '[name]__[local]--[hash:base64:5]'
+						}
+					}
+				]
+			},
+			{
+				test: /\.less$/,
+				use: ['style-loader', 'css-loader', 'less-loader']
+			}
+		]
+	},
+	resolve: {
+		// 因为默认找到的vue库是runtime库，所以要用runtime加编译的
+		alias: {
+			'vue$': 'vue/dist/vue.esm.js'
+		}
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new webpack.BannerPlugin('sdsdsds'),
+		new HtmlWebpackPlugin({
+			template: __dirname + '/app/index.html'
+		})
+	]
+}	
